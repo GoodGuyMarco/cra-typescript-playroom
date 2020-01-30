b84eaeb2dfa04b16736cae48f22428599bc80829
Marco Bartelt <mbartelt@pro-volution.de>
2020-01-30 13:29:48 +0100

    first commit

---
+++ playroom.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  components: './src/components',
+  outputPath: './dist/playroom',
+
+  // Optional:
+  title: 'My Awesome Library',
+  themes: './src/themes',
+  frameComponent: './playroom/FrameComponent.js',
+  widths: [320, 375, 768, 1024],
+  port: 9000,
+  openBrowser: true,
+  exampleCode: `
+    <Button>
+      Hello World!
+    </Button>
+  `,
+  webpackConfig: () => ({
+    // Custom webpack config goes here...
+  })
+};
